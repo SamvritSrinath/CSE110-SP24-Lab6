bfd781f0fb2d0bc065b58e9d95c3b77cc9c5241d
qujessica2048 <qu.jessica2048@gmail.com>
2024-05-14 17:53:21 -0700

    Added more tests

--- __tests__/lab6_crud.test.js
+++ __tests__/lab6_crud.test.js
@@ -6,26 +6,57 @@ describe('Notetaking App', () => {
     });
 
     it('should add a new note', async () => {
-        await page.waitForSelector('.add-note'); // Increase timeout to 10 seconds
-
         // Click the "Add Note" button
         await page.click('.add-note');
 
+        // Get the number of notes in the page
+        const noteCount = await page.$$eval('.note', notes => notes.length);
+
+        // Verify that 1 note is added
+        expect(noteCount).toBe(1);
+    });
+
+    it('should edit the new note', async () => {
         // Wait for the note input to appear
-        await page.click('.note'); // Increase timeout to 10 seconds
+        await page.click('.note');
 
         // Type a new note into the input field
         await page.type('.note', 'Test Note');
 
-        // Click away fro note
-        await page.click('body');
+        // Click tab to save note
+        await page.keyboard.press('Tab');
 
+        // Wait for the note input to appear
+        //await page.waitForSelector('.note');
+        
         // Get the text content of the new note
-        const newNoteContent = await page.$eval('.note', textarea => textarea.value);
+        const newNoteContent = await page.$$eval('.note', textarea => textarea.value);
 
         // Verify that the new note content matches what was typed
-        console.log(newNoteContent);
         expect(newNoteContent).toBe('Test Note');
+    });
+
+    it('should edit the existing note', async () => {
+        // Wait for the note input to appear
+        await page.click('.note');
+
+        // Type a new note into the input field
+        await page.type('.note', 'skibidy');
+
+        // Click tab to save note
+        await page.keyboard.press('Tab');
+
+        // Wait for the note input to appear
+        //await page.waitForSelector('.note');
+
+        // Get the text content of the new note
+        const newNoteContent = await page.$$eval('.note', textarea => textarea.value);
+
+        // Verify that the new note content matches what was typed
+        expect(newNoteContent).toBe('Test Noteskibidy');
         
     });
+
+    
+
 });
